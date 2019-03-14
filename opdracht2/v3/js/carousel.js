b7647bd1bfb3d2fdbc691d3988c7b239c9e3f01f
Csukiii <roy-csuka@live.nl>
2019-03-14 10:31:02 +0100

    v3
    - Readme aangepast
    - CSS wijzigingen voor versie 3
    - Slider aangemaakt

---
+++ opdracht2/v3/js/carousel.js
@@ -0,0 +1,17 @@
+/*jslint devel: true */
+
+var aantalPlaatjes = document.querySelectorAll('section img').length;
+var plaatje = document.querySelector('section img').offsetWidth;
+var widthCarousel = plaatje * aantalPlaatjes;
+
+var section = document.querySelector(' main > section:first-of-type div');
+var knopLinks = document.querySelector('section .links');
+var knopRechts = document.querySelector('section .rechts');
+
+function (plaatje) {
+	if (plaatje = 0) {
+		console.log('doe niks');
+	} else {
+		console.log('doe niks');
+	}
+}
